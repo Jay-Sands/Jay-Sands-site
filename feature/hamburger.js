b2eabd4d78702686992b3d22cd7be09a9069b7c3
Jay <aent1212@gmail.com>
2026-01-19 16:43:39 -0500

    Cleanup: Remove duplicated feature folder and its contents

--- feature/hamburger.js
+++
@@ -1,9 +0,0 @@
-// Hamburger Menu Toggle
-const hamburger = document.querySelector('.hamburger-menu');
-const mobileNav = document.querySelector('.mobile-nav');
-
-if (hamburger && mobileNav) {
-  hamburger.addEventListener('click', () => {
-    mobileNav.classList.toggle('open');
-  });
-}
